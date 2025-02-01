2cc078db170ef79bc161f607f20c9080173f9458
RL-Fields <25580952+RL-Fields@users.noreply.github.com>
2025-02-01 11:33:05 +0000

    Update volumeknob.ino
    This version can now load the interface and scroll between buttons. Clicking the buttons still doesn't work

--- volumeknob.ino
+++ volumeknob.ino
@@ -19,12 +19,12 @@
 // https://github.com/wireless-tag-com/ZX2D10GE01R-V4848
 
 #include <lvgl.h>
-#include <TFT_eSPI.h>
+#include <TFT_eSPI.h> //Must use platform: espressif32@6.5.0
 #include <Arduino_GFX_Library.h>
 #include "button.hpp"
 #include "mt8901.hpp"
 #include <WiFi.h>
-#include <HARestAPI.h>
+#include <HARestAPI.h> // remove the two setfingerprint lines of code from this library
 
 #include "ui.h"
 //Home assistant connector libraries
@@ -41,16 +41,35 @@
 #define ECO_O(y) (y > 0) ? -1 : 1
 #define ECO_STEP(x) x ? ECO_O(x) : 0
 #define GFX_BL 38
-//static uint8_t cnt_5 = -1;
+static uint8_t cnt_5 = -1;
 
-//variables for adjusting the volume
-String entity = "media_player.echo_studio"; //device to control
-int btn_hold_time = 0;
+// SCREEN: ui_SongControls
+void ui_SongControls_screen_init(void);
+extern lv_obj_t * ui_SongControls;
+extern lv_obj_t * ui_MusicArtwork;
+extern lv_obj_t * ui_Artist;
+extern lv_obj_t * ui_SongName;
+extern lv_obj_t * ui_Source;
+extern lv_obj_t * ui_LowerPanel;
+extern lv_obj_t * ui_SongProgressBar;
+void ui_event_ButtonPlayPause(lv_event_t * e);
+extern lv_obj_t * ui_ButtonPlayPause;
+void ui_event_ButtonBack(lv_event_t * e);
+extern lv_obj_t * ui_ButtonBack;
+extern lv_obj_t * ui_ButtonShuffle;
+void ui_event_ButtonNext(lv_event_t * e);
+extern lv_obj_t * ui_ButtonNext;
+extern lv_obj_t * ui_ButtonRepeat;
+extern lv_obj_t * ui_DisplaySource;
+
+extern lv_group_t *lv_group; // Declare lv_group globally
+
+int previousEncoderValue = 0; // Initialize the previous encoder value
+int degreesPerStep = 7; // Set the degrees for each step
 bool is_button_held = false;
 bool toggle_volume_mode = false;
-int previousEncoderValue = 0; // Initialize the previous encoder value
-
-int degreesPerStep = 7; // Set the degrees for each step
+int btn_hold_time = 0;
+static uint8_t cnt_5 = -1;// what is this
 
 
 WiFiClient client;
@@ -58,11 +77,11 @@ WiFiClient client;
 HARestAPI ha(client);
 
 //Details from Home Assistant
-
+String entity = "media_player.adam_s_echo_studio"; //device to control
 
 const char* ha_ip = "homeassistant.local";
 uint16_t ha_port = 8123; // Could be 443 is using SSL
-const char* ha_pwd = "xxxxxxx";  //long-lived password. On HA, Profile > Long-Lived Access Tokens > Create Token
+const char* ha_pwd = "xxx";  //long-lived password. On HA, Profile > Long-Lived Access Tokens > Create Token
 
 
 
@@ -70,8 +89,8 @@ HADevice device("myVolumeKnob");
 //HAMqtt mqtt(client, device);
 
 //CONNECT TO WIFI
-const char* ssid = "xxxxx";
-const char* password = "xxxxxx";
+const char* ssid = "xxx";
+const char* password = "xxx";
 
 // Static IP configuration
 IPAddress staticIP(192, 168, 1, 100); // ESP32 static IP
@@ -134,13 +153,13 @@ static const uint16_t screenHeight = 480;
 //static uint32_t screenHeight; //const &added =480 for ui
 static lv_disp_draw_buf_t draw_buf;
 static lv_color_t buf[ screenWidth * screenHeight / 10 ]; // changed for ui originally was *disp_draw_buf;
-//static lv_color_t *disp_draw_buf;
-static lv_group_t *lv_group;
+static lv_color_t *disp_draw_buf;
+//static lv_group_t *lv_group;
 //static lv_obj_t *lv_btn_1;
 //static lv_obj_t *lv_btn_2;
-static lv_obj_t *lv_btn_3;
-static lv_obj_t *lv_btn_4;
-static lv_obj_t *lv_btn_5;
+//static lv_obj_t *lv_btn_3;
+//static lv_obj_t *lv_btn_4;
+//static lv_obj_t *lv_btn_5;
 
 
 TFT_eSPI tft = TFT_eSPI(screenWidth, screenHeight); /* TFT instance added for ui */
@@ -183,12 +202,38 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
 //in the original code
   lv_disp_flush_ready(disp);
 }
-
 void encoder_read(lv_indev_drv_t *drv, lv_indev_data_t *data) {
   static int16_t cont_last = 0;
   int16_t cont_now = mt8901_get_count();
   data->enc_diff = ECO_STEP(cont_now - cont_last);
   cont_last = cont_now;
+
+  if (data->enc_diff > 0) {
+       lv_group_focus_next(lv_group);  // Move focus to the next object
+    } else if (data->enc_diff < 0) {
+        lv_group_focus_prev(lv_group);  // Move focus to the previous object
+    }
+
+//  lv_obj_t* focused_obj = lv_group_get_focused(lv_group);  // Get the currently focused object
+
+//    if (data->enc_diff > 0) {
+//        lv_group_focus_next(lv_group);  // Move focus to the next object
+
+        // Check if the current object is the last one in the group, if so, focus the first object
+//        if (focused_obj == ui_ButtonRepeat) {
+             // Get the first object in the group
+//            lv_group_focus_obj(ui_ButtonShuffle);  // Focus the first object
+ //       }
+ //   } else if (data->enc_diff < 0) {
+ //       lv_group_focus_prev(lv_group);  // Move focus to the previous object
+
+        // Check if the current object is the first one in the group, if so, focus the last object
+  //      if (focused_obj == ui_ButtonShuffle) {
+              // Get the last object
+   //         lv_group_focus_obj(ui_ButtonRepeat);  // Focus the last object
+    //    }
+   // }
+
   if (button_isPressed(g_btn)) {
     data->state = LV_INDEV_STATE_PR;
   } else {
@@ -196,8 +241,13 @@ void encoder_read(lv_indev_drv_t *drv, lv_indev_data_t *data) {
   }
 }
 
+
+
 void setup() {
   Serial.begin(115200);
+    delay(2000);  // Wait for serial monitor to start
+
+
   device.enableSharedAvailability();
   device.enableLastWill();
 
@@ -257,19 +307,45 @@ void setup() {
   Serial.println( LVGL_Arduino );
   Serial.println( "I am LVGL_Arduino" );
 
-//original code
+    // Check heap memory
+  Serial.print("Free heap memory before allocation: ");
+  Serial.println(ESP.getFreeHeap());
+
+  tft.begin();          /* TFT init */
+  tft.setRotation( 3 ); /* Landscape orientation, flipped */
+  Serial.println( "tft Initialised" );
+
+  // Must use PSRAM In original code
+disp_draw_buf = (lv_color_t*)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * screenHeight / 10, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+  if (!disp_draw_buf) {
+    Serial.println("Failed to allocate memory for drawing buffer");
+    return;  // Stop further execution if memory allocation fails
+  }
+
+  lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * screenHeight / 10);
+
+  // Initialize LVGL
   lv_init();
 
 //added for ui
-#if LV_USE_LOG != 0
+  #if LV_USE_LOG != 0
     lv_log_register_print_cb( my_print ); /* register print function for debugging */
-#endif
+  #endif
 
- tft.begin();          /* TFT init */
-    tft.setRotation( 3 ); /* Landscape orientation, flipped */
 
-    lv_disp_draw_buf_init( &draw_buf, buf, NULL, screenWidth * screenHeight / 10 );
-    Serial.println( "tft Initialised" );
+ 
+    
+
+        /* Initialize the display */
+  static lv_disp_drv_t disp_drv;
+  lv_disp_drv_init(&disp_drv);
+    /* Change the following line to your display resolution */
+  disp_drv.hor_res = screenWidth;
+  disp_drv.ver_res = screenHeight;
+  disp_drv.flush_cb = my_disp_flush;
+  disp_drv.draw_buf = &draw_buf;
+  lv_disp_drv_register(&disp_drv);
+  Serial.println( "Display Initialised" );
 
 //original code
 
@@ -280,28 +356,16 @@ void setup() {
   // Magnetic Encoder
   mt8901_init(5, 6);
 
+  lv_obj_set_event_cb(g_btn, btn_event_cb);
+
 //in original code
   //screenWidth = gfx->width();
   //screenHeight = gfx->height();
 
-  // Must use PSRAM In original code
-//  disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * 32, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
-
-//  if (!disp_draw_buf) {
-//    Serial.println("LVGL disp_draw_buf allocate failed!");
-//  } else {
-//    lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * 32);
-
-    /* Initialize the display */
-    static lv_disp_drv_t disp_drv;
-    lv_disp_drv_init(&disp_drv);
-    /* Change the following line to your display resolution */
-    disp_drv.hor_res = screenWidth;
-    disp_drv.ver_res = screenHeight;
-    disp_drv.flush_cb = my_disp_flush;
-    disp_drv.draw_buf = &draw_buf;
-    lv_disp_drv_register(&disp_drv);
-    Serial.println( "Display Initialised" );
+  // Create a new group
+  lv_group = lv_group_create();
+  lv_group_set_default(lv_group);
+  Serial.println( "lv_group Initialised" );
 
     /* Initialize the input device driver */
     static lv_indev_drv_t indev_drv;
@@ -311,8 +375,8 @@ void setup() {
     lv_indev_drv_register(&indev_drv);
     Serial.println( "Input Driver Initialised" );
 
-    init_lv_group();
-    Serial.println( "lv_group Initialised" );
+    //init_lv_group();
+    //Serial.println( "lv_group Initialised" );
 
     //ui code
     ui_init();
@@ -323,8 +387,21 @@ void setup() {
     //Serial.println( "Song control screen initialised" );
 
 //    lv_example_get_started_1();
-//    lv_image_btn_events();
-//    Serial.println( "Buttons Initialised" );
+
+      ui_SongControls_screen_init();
+
+      Serial.println("Adding event callback for Buttons");
+      lv_obj_add_event_cb(ui_ButtonPlayPause, btn_event_cb, LV_EVENT_CLICKED, NULL);
+      lv_obj_add_event_cb(ui_ButtonBack, btn_event_cb, LV_EVENT_CLICKED, NULL);
+      lv_obj_add_event_cb(ui_ButtonNext, btn_event_cb, LV_EVENT_CLICKED, NULL);
+      Serial.println( "Song Control" );
+      lv_group_focus_obj(ui_ButtonPlayPause);
+
+    if (ui_ButtonPlayPause == NULL || ui_ButtonBack == NULL || ui_ButtonNext == NULL) {
+    Serial.println("Buttons are not initialized!");
+    return;
+}
+
 
 
   //}
@@ -336,23 +413,24 @@ void setup() {
 
 void loop() {
   lv_timer_handler(); /* let the GUI do its work */
+  lv_task_handler();
 
-  if (toggle_volume_mode == true) {
-    int encoderChange = mt8901_get_count();
+ // if (toggle_volume_mode == true) {
+ //   int encoderChange = mt8901_get_count();
       // Check if the change in encoder value corresponds to a 7-degree increment
-    if ((encoderChange - previousEncoderValue) >= degreesPerStep) {
-        ha.sendCustomHAData("/api/services/media_player/volume_down", "{\"entity_id\":\"" + entity + "\"}");
+ //   if ((encoderChange - previousEncoderValue) >= degreesPerStep) {
+ //       ha.sendCustomHAData("/api/services/media_player/volume_down", "{\"entity_id\":\"" + String(entity) + "\"}");
         // Update the previous encoder value
-        previousEncoderValue = previousEncoderValue + 7;
-    }
-    if ((encoderChange - previousEncoderValue) <= -degreesPerStep) {
-        ha.sendCustomHAData("/api/services/media_player/volume_up", "{\"entity_id\":\"" + entity + "\"}");
+ //       previousEncoderValue = previousEncoderValue + 7;
+ //   }
+ //   if ((encoderChange - previousEncoderValue) <= -degreesPerStep) {
+ //       ha.sendCustomHAData("/api/services/media_player/volume_up", "{\"entity_id\":\"" + String(entity) + "\"}");
         // Update the previous encoder value
-        previousEncoderValue = previousEncoderValue - 7;
-    }
+ //       previousEncoderValue = previousEncoderValue - 7;
+ //   }
       // Add a delay to control the frequency of readings (adjust as needed)
      delay(100); // Delay for 100 milliseconds before the next check
-  }  
+    
 
 
   server.handleClient();
@@ -360,111 +438,35 @@ void loop() {
   delay(5);
 }
 
-void init_lv_group() {
-  lv_group = lv_group_create();
-  lv_group_set_default(lv_group);
-
-  lv_indev_t *cur_drv = NULL;
-  for (;;) {
-    cur_drv = lv_indev_get_next(cur_drv);
-    if (!cur_drv) {
-      break;
-    }
-
-    if (cur_drv->driver->type == LV_INDEV_TYPE_ENCODER) {
-      lv_indev_set_group(cur_drv, lv_group);
-    }
-  }
-}
-
-
-// Function to handle button press -now in external file
-//static void btn_event_cb(lv_event_t * e) {
- // }
-
-
-/**
- * Create a button with a label and react on click event.
- */
-//void lv_image_btn_events(void) {
-
- // lv_obj_add_event_cb(ui_ButtonPlayPause, btn_event_cb, LV_EVENT_ALL, NULL);
- // lv_obj_add_event_cb(ui_ButtonBack, btn_event_cb, LV_EVENT_ALL, NULL);
- // lv_obj_add_event_cb(ui_ButtonNext, btn_event_cb, LV_EVENT_ALL, NULL);
-
-//}
-//void lv_example_get_started_1(void) {
-
-  //BUTTON THREE
-//  lv_btn_3 = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
-//  lv_obj_set_style_bg_color(lv_btn_3, lv_palette_main(LV_PALETTE_GREEN), LV_PART_MAIN); /*Set its main color*/
-
-//  lv_obj_set_size(lv_btn_3, 80, 80); /*Set its size*/
-//  lv_obj_align(lv_btn_3, LV_ALIGN_CENTER, -80, 0); /*Set its position*/
-  //lv_obj_add_event_cb(ui_ButtonBack, btn_event_cb, LV_EVENT_ALL, NULL); /*Assign a callback to the button*/ //previously called lv_btn_3
-
-//  lv_obj_t * label3 = lv_label_create(lv_btn_3); /*Add a label to the button*/
-//  lv_label_set_text(label3, "Previous"); /*Set the labels text*/
-//  lv_obj_center(label3);
-
-  //BUTTON FOUR
-//  lv_btn_4 = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
-//  lv_obj_set_style_bg_color(lv_btn_4, lv_palette_main(LV_PALETTE_GREEN), LV_PART_MAIN); /*Set its main color*/
-
-//  lv_obj_set_size(lv_btn_4, 80, 80); /*Set its size*/
-//  lv_obj_align(lv_btn_4, LV_ALIGN_CENTER, 80, 0); /*Set its position*/
- // lv_obj_add_event_cb(ui_ButtonPlayPause, btn_event_cb, LV_EVENT_ALL, NULL); /*Assign a callback to the button*///previously called lv_btn_4
-
-//  lv_obj_t * label4 = lv_label_create(lv_btn_4); /*Add a label to the button*/
-//  lv_label_set_text(label4, "Next"); /*Set the labels text*/
-//  lv_obj_center(label4);
-
-  //BUTTON FIVE
-//  lv_btn_5 = lv_btn_create(lv_scr_act()); /*Add a button the current screen*/
-//  lv_obj_set_style_bg_color(lv_btn_5, lv_palette_main(LV_PALETTE_DEEP_ORANGE), LV_PART_MAIN); /*Set its main color*/
-
-//  lv_obj_set_size(lv_btn_5, 80, 80); /*Set its size*/
-//  lv_obj_align(lv_btn_5, LV_ALIGN_CENTER, 0, 0); /*Set its position*/
-//  lv_obj_add_event_cb(lv_btn_5, btn_event_cb, LV_EVENT_ALL, NULL); /*Assign a callback to the button*/
-
-//  lv_obj_t * label5 = lv_label_create(lv_btn_5); /*Add a label to the button*/
-//  lv_label_set_text(label5, "Play/Pause"); /*Set the labels text*/
-//  lv_obj_center(label5);
-
-//}
-
 
 void btn_event_cb(lv_event_t * e)
 {
 	lv_event_code_t code = lv_event_get_code(e);
   lv_obj_t * btn = lv_event_get_target(e);
+  Serial.print("Event triggered: ");
+  Serial.println(code);
 
-
-
-
-
-
-  if (code == LV_EVENT_PRESSED) {
-    btn_hold_time = millis(); // Record the time when the button is pressed
-    Serial.print("Button hold time: ");
-    Serial.println(btn_hold_time);
-    is_button_held = false; // Reset the flag for held status
-  } else if (code == LV_EVENT_RELEASED) {
-    uint32_t release_time = millis();
-    if ((release_time - btn_hold_time) >= 1200) { // Check if the button was held for at least 1.2 seconds
-      is_button_held = true; // Set the flag indicating the button was held
-      if(toggle_volume_mode == false){
-          toggle_volume_mode = true;
-          previousEncoderValue = mt8901_get_count();
-          }
-      else {
-          toggle_volume_mode = false;
-          } 
+//  if (code == LV_EVENT_PRESSED) {
+//    btn_hold_time = millis(); // Record the time when the button is pressed
+//    Serial.print("Button hold time: ");
+//    Serial.println(btn_hold_time);
+//    is_button_held = false; // Reset the flag for held status
+//  } else if (code == LV_EVENT_RELEASED) {
+//    uint32_t release_time = millis();
+//    if ((release_time - btn_hold_time) >= 1200) { // Check if the button was held for at least 1.2 seconds
+//      is_button_held = true; // Set the flag indicating the button was held
+//      if(toggle_volume_mode == false){
+//          toggle_volume_mode = true;
+//          previousEncoderValue = mt8901_get_count();
+  //        }
+   //   else {
+  //        toggle_volume_mode = false;
+  //        } 
       
       // Toggle the mode
-      Serial.print("Volume mode is: ");
-      Serial.println(toggle_volume_mode);
-    } 
+//      Serial.print("Volume mode is: ");
+//      Serial.println(toggle_volume_mode);
+//    } 
   //Serial.print("Button held is: ");
   //Serial.println(is_button_held);
   //if (is_button_held) {
@@ -472,29 +474,27 @@ void btn_event_cb(lv_event_t * e)
   
     // Change volume
     // Handle volume change based on encoder movement
-  } if (toggle_volume_mode == false){
+//  } if (toggle_volume_mode == false){
       //lv_scr_load(ui_SongControls);
       //exit_loop = true; // Set the flag to exit the loop
-      if (code == LV_EVENT_CLICKED && is_button_held == false) {
+      if (code == LV_EVENT_CLICKED){ // && is_button_held == false) {
         // Control play/pause or track selection
 
         if (btn == ui_ButtonBack) { //previously called lv_btn_3
-          //Serial.println("lv_btn_3 pressed\n"); // Print when lv_btn_3 is pressed
+          Serial.println("lv_btn_3 pressed\n"); // Print when lv_btn_3 is pressed
         //static uint8_t cnt_3 = 0;
           ha.sendCustomHAData("/api/services/media_player/media_previous_track", "{\"entity_id\":\"" + entity + "\"}");
 
         } else if (btn == ui_ButtonNext) { //previously called lv_btn_4
         //static uint8_t cnt_4 = 0;
           ha.sendCustomHAData("/api/services/media_player/media_next_track", "{\"entity_id\":\"" + entity + "\"}");
-        //  Serial.println("lv_btn_4 pressed\n"); // Print when lv_btn_4 is pressed
+          Serial.println("lv_btn_4 pressed\n"); // Print when lv_btn_4 is pressed
 
         } else if (btn == ui_ButtonPlayPause) {
+          cnt_5 = cnt_5 * -1;
           ha.sendCustomHAData("/api/services/media_player/media_play_pause", "{\"entity_id\":\"" + entity + "\"}");
           //ha.sendCustomHAData("/api/services/media_player/media_play_pause", "{\"entity_id\":\"media_player.adam_s_echo_studio\"}");
-        //  Serial.println("lv_btn_5 pressed\n"); // Print when lv_btn_5 is pressed
+          Serial.println("lv_btn_5 pressed\n"); // Print when lv_btn_5 is pressed
        }
      }
   }
- // else if (toggle_volume_mode == true) {    
- //    lv_scr_load(ui_VolumeKnob);
-}
